7ac4dceeb5ff9578aad716b389dd55780ea04436
Philip Andersson <phandersson@icloud.com>
2016-03-31 22:07:11 +0200

    updates

---
+++ TODO.md
@@ -0,0 +1,20 @@
+# Setup Todo
+
+### Material Design
+- [x] Base layout
+- [ ] Colors
+- [ ] Element Implementation
+
+### ES6
+
+###### Modules
+- [x] Login
+- [ ] Notifications
+- [ ] Calendar
+- [x] Icons
+- [ ] API URL Parameter (rootScope or something)
+
+###### Reusables
+- [ ] ListControllers (tables)
+- [ ] OrderRowControllers (invoices and orders)
+- [ ] Form Controllers (contact, supplier etc)
--- src/app/app.config.js
+++ src/app/app.config.js
@@ -1,10 +1,12 @@
 import authenticationInterceptor from '../components/auth/interceptor';
 
-export default function routing($urlRouterProvider, $locationProvider, $authProvider, $httpProvider) {
+export default function routing($urlRouterProvider, $locationProvider, $authProvider, $httpProvider, $mdIconProvider) {
     $locationProvider.html5Mode(true);
     $urlRouterProvider.otherwise('dashboard');
+
+    $mdIconProvider.defaultIconSet('img/mdi.svg');
 
     $authProvider.loginUrl = 'https://system.cirons.com/sample/api/v1/auth';
     $httpProvider.interceptors.push(authenticationInterceptor);
 }
-routing.$inject = ['$urlRouterProvider', '$locationProvider', '$authProvider', '$httpProvider'];
+routing.$inject = ['$urlRouterProvider', '$locationProvider', '$authProvider', '$httpProvider', '$mdIconProvider'];
