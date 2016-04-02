6918fb511eff5c3804d4f311c1fc3861e02197ae
Philip Andersson <phandersson@icloud.com>
2016-04-03 01:17:40 +0200

    notificaiton bar, c3 chart and main chart

--- src/app/app.js
+++ src/app/app.js
@@ -6,6 +6,10 @@ import satellizer from 'satellizer';
 import ngCookies from 'angular-cookies';
 import ngAria from 'angular-aria';
 
+window.d3 = require('d3');
+window.c3 = require('c3');
+require('c3-angular');
+
 import AppController from './app.controller';
 import run from './run';
 
@@ -15,6 +19,7 @@ import services from '../services';
 import components from '../components';
 import directives from '../directives';
 
+import '../../node_modules/c3/c3.min.css';
 import '../style/app.scss';
 
 let app = () => {
@@ -27,7 +32,7 @@ let app = () => {
 
 const MODULE_NAME = 'app';
 
-angular.module(MODULE_NAME, [uirouter, services, components, directives, ngMaterial, ngCookies, satellizer, ngAria])
+angular.module(MODULE_NAME, [uirouter, services, components, directives, ngMaterial, ngCookies, satellizer, ngAria, 'gridshore.c3js.chart'])
     .run(run)
     .config(routing)
     .config(theme)
